7798945b0387a839f85e9b066737727c8f69b32c
Igor Kornilov <gaber32188@gmail.com>
2020-02-02 12:43:37 +0300

    Made celss editable

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { data } from "/src/data.js";
 const table = document.querySelector('.content-table');
 const tbody = table.querySelector('tbody');
 
+// console.log(tds);
 
 function addRow() {
     data.forEach(row => {
@@ -10,7 +11,7 @@ function addRow() {
         // Making an array from given object
         const entries = Object.entries(row);
     
-        //Getting the each individual cell
+        //Getting each individual cell
         entries.forEach(cell => {
             const td = document.createElement('td');
             // Need to create some sort of check here to place the value on the corresponding cell based on the heading of the row
@@ -26,6 +27,23 @@ function clearRows() {
         tbody.removeChild(tbody.firstChild);
     }
 }
-addRow();
-// document.addEventListener('DOMContentLoaded', clearRows);
+
+function loadTable() {
+    return new Promise(function (resolve, reject) {
+        addRow();
+        resolve()
+    });
+}
+
+loadTable().then(function() {
+    const tds = document.querySelectorAll('td');
+    tds.forEach(td => {
+        td.setAttribute('contenteditable', true);
+    });
+});
+
+
+
+// document.addEventListener('DOMContentLoaded', addRow);
+// tds.forEach(td => td.isContentEditable = true);
 
